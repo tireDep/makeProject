c2b56157fe841fb321d953b0b7331a8e87bd26d7
tireDep <wing997@naver.com>
2018-07-04 21:03:38 +0900

    재업로드

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+    public static int gridWidth = 10;
+    public static int gridHeight = 20;  
+    // 테트리스 게임 화면 크기
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public bool CheckInsideGrid(Vector2 pos)    // 창 안에 있는지 유무
+    {
+        return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+        // 가로가 0보다 크거나 최대크기보다 작음 & 세로가 최소크기보다 크거나 같음
+    }
+
+    public Vector2 Round(Vector2 pos)   // 위치값 올림
+    {
+        return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+        // 해당 위치 값 올림
+    }
+}
---
+++ Assets/Scripts/Tetromino.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetromino : MonoBehaviour {
+    float fall = 0;
+    public float fallSpeed = 1; // 떨어지는 속도
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckUserInput();
+	}
+
+    void CheckUserInput()   // 게임 실행 관련
+    {
+        if(Input.GetKeyDown(KeyCode.RightArrow))    // 오른쪽 화살표 입력
+        {
+            transform.position += new Vector3(1, 0, 0);
+
+            if(CheckIsValidPosition()){
+            }else
+                transform.position += new Vector3(-1, 0, 0);
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftArrow)) // 왼쪽 화살표 입력
+        {
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (CheckIsValidPosition()){
+            }else
+                transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow)) // 위쪽 화살표 입력
+        {
+            transform.Rotate(0, 0, 90); // 90도 회전
+
+            if(CheckIsValidPosition()){
+            }else
+                transform.Rotate(0, 0, -90);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time-fall>=fallSpeed) // 아래쪽 화살표 입력 or 자동으로 한칸씩 떨어짐
+        {
+            transform.position += new Vector3(0, -1, 0);    // 아래로 한 칸 이동
+
+            if (CheckIsValidPosition()){
+            }else
+                transform.position += new Vector3(0, 1, 0);
+
+            fall = Time.time;   // 떨어지는 속도 증가!
+            // 시간 - 지난 시간 >= 떨어지는 속도
+        }
+        // 스페이스바 -> 한번에 떨어지는 것 추가하기
+    }
+
+    bool CheckIsValidPosition() // 올바른 위치인지 판별
+    {
+        foreach(Transform Block in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(Block.position);
+
+            if (FindObjectOfType<Game>().CheckInsideGrid(pos) == false)
+                return false;
+        }
+        return true;
+    }
+}
