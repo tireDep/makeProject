128dfeab283819e228b231e9c072c013eeaf992b
tireDep <wing997@naver.com>
2018-07-05 16:52:29 +0900

    * 수정 목록 - 블록이 다른 블록을 뚫지않고 쌓을 수 있음 - 각각의 블록들마다 회전 정보 입력(회전 제한 등)

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,16 +1,20 @@
-﻿using System.Collections;
+﻿// 실제적인 게임 실행 클래스, grid에 적용됨
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour {
 
     public static int gridWidth = 10;
-    public static int gridHeight = 20;  
-    // 테트리스 게임 화면 크기
+    public static int gridHeight = 20;
+    // 테트리스 게임 화면 크기(블록 공간)
+
+    public static Transform[,] grid = new Transform[gridWidth, gridHeight];
+    // 각각의 블록이 차지하는 공간 변수
 
 	// Use this for initialization
 	void Start () {
-        SpawnNextTetromino();
+        SpawnNextTetromino();   // 랜덤으로 블록 자동 생성
 	}
 	
 	// Update is called once per frame
@@ -18,27 +22,64 @@ public class Game : MonoBehaviour {
 		
 	}
 
+    public void UpdateGrid(Tetromino tetromino) // 전체 공간 계산(남은공간)
+    {
+        for(int y=0; y<gridHeight;++y)
+        {
+            for(int x=0; x<gridWidth; ++x)
+            {
+                if(grid[x,y]!=null)
+                {
+                    if (grid[x, y].parent == tetromino.transform)
+                    {
+                        grid[x, y] = null;
+                    }
+                }
+            }
+        }
+
+        foreach(Transform mino in tetromino.transform)  // 블록이 차지하고 있는 공간 계산
+        {
+            Vector2 pos = Round(mino.position);
+
+            if(pos.y<gridHeight)
+            {
+                grid[(int)pos.x, (int)pos.y] = mino;
+            }
+        }
+    }   // 함수 끝
+
+    public Transform GetTransformAtGridPosition(Vector2 pos)    // 블록이 차지하고 있는 공간 계산
+    {
+        if (pos.y > gridHeight - 1)
+            return null;
+        else
+            return grid[(int)pos.x, (int)pos.y];
+    }   // 함수 끝
+
     public void SpawnNextTetromino()    // 다음 블록 생성
     {
         GameObject nextTetromino = (GameObject)Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);
-    }
+        // 랜덤으로 다음 블록 생성, 위치정보, 회전정보
+    }   // 함수 끝
 
     public bool CheckInsideGrid(Vector2 pos)    // 창 안에 있는지 유무
     {
         return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
         // 가로가 0보다 크거나 최대크기보다 작음 & 세로가 최소크기보다 크거나 같음
-    }
+    }   // 함수 끝
 
-    public Vector2 Round(Vector2 pos)   // 위치값 올림
+    public Vector2 Round(Vector2 pos)   // 위치값 반올림
     {
         return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
-        // 해당 위치 값 올림
-    }
+        // 해당 위치 값 반올림
+    }   // 함수 끝
 
-    string GetRandomTetromino()
+    string GetRandomTetromino() // 랜덤 블록 생성
     {
         int randomTetromino = Random.Range(1, 8);   // 랜덤 지정
         string randomTetrominoName = "Prefabs/TBlock";  // 기본 지정 블록
+        // 프리팹 내부에 블록이 존재하기 때문에 Prefabs/추가
 
         switch(randomTetromino)
         {
@@ -65,5 +106,6 @@ public class Game : MonoBehaviour {
                 break;
         }
         return randomTetrominoName;
-    }
+    }   // 함수 끝
+
 }
--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -1,13 +1,15 @@
-﻿using System.Collections;
+﻿// 블록관련 클래스, 각각의 블록에 적용됨
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tetromino : MonoBehaviour {
-    float fall = 0;
-    public float fallSpeed = 1; // 떨어지는 속도
+    float fall = 0; // 낙하 속도 관련 변수
+    public float fallSpeed = 1; // 떨어지는 속도 변수, 유니티에서 직접 수정 가능
 
     public bool allowRoatation = true;
     public bool limitRoatiotion = false;
+    // 회전 관련 변수, 유니티에서 직접 수정 가능
 
 	// Use this for initialization
 	void Start () {
@@ -15,35 +17,37 @@ public class Tetromino : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {    // 프레임 당 실행
         CheckUserInput();
-	}
+	}   // 함수 끝
 
-    void CheckUserInput()   // 게임 실행 관련
+    void CheckUserInput()   // 게임 입력 함수
     {
         if(Input.GetKeyDown(KeyCode.RightArrow))    // 오른쪽 화살표 입력
         {
             transform.position += new Vector3(1, 0, 0);
 
-            if(CheckIsValidPosition()){
-            }else
+            if(CheckIsValidPosition()) // 존재하고 있는지 확인
+                FindObjectOfType<Game>().UpdateGrid(this); // 공간 계산
+            else
                 transform.position += new Vector3(-1, 0, 0);
         }
         else if(Input.GetKeyDown(KeyCode.LeftArrow)) // 왼쪽 화살표 입력
         {
             transform.position += new Vector3(-1, 0, 0);
 
-            if (CheckIsValidPosition()){ }
+            if (CheckIsValidPosition())  // 존재하고 있는지 확인 
+                FindObjectOfType<Game>().UpdateGrid(this); // 공간계산
             else
                 transform.position += new Vector3(1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow)) // 위쪽 화살표 입력
         {
-            if (allowRoatation)
+            if (allowRoatation) // 회전 가능
             {
-                if (limitRoatiotion)
+                if (limitRoatiotion)    // 제한된 회전 -> S블록 등은 회전 제한
                 {
-                    if(transform.rotation.eulerAngles.z>=90)
+                    if(transform.rotation.eulerAngles.z>=90)    // z축 회전 각이 90이상
                         transform.Rotate(0, 0, -90);
                     else
                         transform.Rotate(0, 0, 90);
@@ -51,19 +55,16 @@ public class Tetromino : MonoBehaviour {
                 else
                     transform.Rotate(0, 0, 90); // 90도 회전
 
-               if (CheckIsValidPosition()){ }
+               if (CheckIsValidPosition()) // 존재하고 있는지 확인 
+                    FindObjectOfType<Game>().UpdateGrid(this); // 공간계산
                 else
                 {
                     if (limitRoatiotion)
                     {
                         if (transform.rotation.eulerAngles.z >= 90)
-                        {
                             transform.Rotate(0, 0, -90);
-                        }
                         else
-                        {
                             transform.Rotate(0, 0, 90);
-                        }
                     }
                     else
                         transform.Rotate(0, 0, -90);
@@ -74,22 +75,21 @@ public class Tetromino : MonoBehaviour {
         {
             transform.position += new Vector3(0, -1, 0);    // 아래로 한 칸 이동
 
-            if (CheckIsValidPosition()){ }
+            if (CheckIsValidPosition()) // 존재하고 있는지 확인 
+                FindObjectOfType<Game>().UpdateGrid(this);  // 공간계산
             else
             {
                 transform.position += new Vector3(0, 1, 0);
-                enabled = false;
-                FindObjectOfType<Game>().SpawnNextTetromino();
+                enabled = false;    // 실행 유무..?
+                FindObjectOfType<Game>().SpawnNextTetromino();  // 다음 블록 자동 생성
             }
-                
-
             fall = Time.time;   // 떨어지는 속도 증가!
             // 시간 - 지난 시간 >= 떨어지는 속도
         }
         // 스페이스바 -> 한번에 떨어지는 것 추가하기
-    }
+    }   // 함수 끝
 
-    bool CheckIsValidPosition() // 올바른 위치인지 판별
+    bool CheckIsValidPosition() // 존재 유무, 올바른 위치인지 판별
     {
         foreach(Transform Block in transform)
         {
@@ -97,7 +97,11 @@ public class Tetromino : MonoBehaviour {
 
             if (FindObjectOfType<Game>().CheckInsideGrid(pos) == false)
                 return false;
+
+            if (FindObjectOfType<Game>().GetTransformAtGridPosition(pos) != null && FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform)
+                return false;
         }
         return true;
-    }
+    }   // 함수 끝
+
 }
