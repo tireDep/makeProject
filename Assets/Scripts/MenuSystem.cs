af1eaf9f53290daf2cc4c08e09f75a593255f8bb
tireDep <wing997@naver.com>
2018-07-10 18:28:42 +0900

    버전관리 파일 추가

---
+++ Assets/Scripts/MenuSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;  // Level 씬 불러오기 위해 사용
+using UnityEngine;
+
+public class MenuSystem : MonoBehaviour {
+    // 환경 설정이기 때문에 start(), update() 필요 없으므로 삭제함
+
+    public void PlayStart()
+    {
+        SceneManager.LoadScene("Level");    // 게임 시작
+    }
+
+    public void fin()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; // play모드를 false로
+#elif UNITY_WEBPLAYER
+    Application.OpenURL("http://google.com");   // 구글 웹으로 전화
+#else
+        Application.Quit();
+#endif
+
+        Application.Quit(); // 일반 응용프로그램, 모바일 처리 가능 but 웹이나 유니티 에디터 동작 x
+    }
+}
