001ec3b07078b9e6caa25fe6c5535c931388b7ce
tireDep <wing997@naver.com>
2018-07-11 16:20:23 +0900

    - 버튼 누르고 있을 경우 내려가지 않던 문제 수정

--- Assets/Scripts/Tetromino.cs
+++ Assets/Scripts/Tetromino.cs
@@ -27,7 +27,8 @@ public class Tetromino : MonoBehaviour {
 
     private float verticalTimer = 0;
     private float horizontalTimer = 0;
-    private float buttonDownWaitTimer = 0;
+    private float buttonDownWaitTimerHorizontal = 0;
+    private float buttonDownWaitTimerVertical = 0;
     // 타이머 변수들
 
     private bool moveImmediateHorizontal = false;
@@ -60,157 +61,183 @@ public class Tetromino : MonoBehaviour {
 
     void CheckUserInput()   // 게임 입력 함수
     {
-        if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.DownArrow))    // 키를 놓았을 때, 타이머 변수 초기화
+        if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))    // 키를 놓았을 때, 타이머 변수 초기화
         {
             moveImmediateHorizontal = false;
             moveImmediateVertical = false;
 
             horizontalTimer = 0;
             verticalTimer = 0;
-            buttonDownWaitTimer = 0;
+            buttonDownWaitTimerHorizontal = 0;
+        }
+
+        if(Input.GetKeyUp(KeyCode.DownArrow))   
+        {
+            moveImmediateVertical = false;
+            verticalTimer = 0;
+            buttonDownWaitTimerVertical = 0;
         }
 
         if(Input.GetKeyDown(KeyCode.Escape))
-        {
             SceneManager.LoadScene("GameOver");
-        }
 
+        // else if -> if로 수정 => if문이어야지만 좌우로 계속 누를 때 고정되지 않음(else if의 경우 안됨)
         if(Input.GetKey(KeyCode.RightArrow))    // 오른쪽 화살표 입력
         {
-            if (moveImmediateHorizontal)    // 계속 누르고 있을 때
-            {
-                if (buttonDownWaitTimer < buttonDownWaitMax)   // 딜레이 시간
-                {
-                    buttonDownWaitTimer += Time.deltaTime;
-                    return;
-                }
-
-                if (horizontalTimer < continuousHorizontalSpeed) // 움직인 시간(누른 시간)에 따라서 속력이 결정됨 
-                {
-                    horizontalTimer += Time.deltaTime;
-                    return; // 끝남을 의미함
-                }
-            }
-            if (!moveImmediateHorizontal)   // 한번만 누를 경우
-                moveImmediateHorizontal = true;
-
-            horizontalTimer = 0;    // 초기화
-
-            transform.position += new Vector3(1, 0, 0);
-
-            if(CheckIsValidPosition()) // 존재하고 있는지 확인
-                FindObjectOfType<Game>().UpdateGrid(this); // 공간 계산
-            else
-                transform.position += new Vector3(-1, 0, 0);
+            moveRight();
         }   // 오른쪽 끝
 
-        else if(Input.GetKey(KeyCode.LeftArrow)) // 왼쪽 화살표 입력
+        if(Input.GetKey(KeyCode.LeftArrow)) // 왼쪽 화살표 입력
         {
-            if(moveImmediateHorizontal) // 계속 누르고 있을 때
-            {
-                if (buttonDownWaitTimer < buttonDownWaitMax)    // 딜레이 시간
-                {
-                    buttonDownWaitTimer += Time.deltaTime;
-                    return;
-                }
-
-                if (horizontalTimer < continuousHorizontalSpeed) // 움직인 시간(누른 시간)에 따라서 속력이 결정됨 
-                {
-                    horizontalTimer += Time.deltaTime;
-                    return; // 끝남을 의미함
-                }
-            }
-            if (!moveImmediateHorizontal)   // 한번만 누를 경우
-                moveImmediateHorizontal = true;
-
-            horizontalTimer = 0;    // 초기화
-
-            transform.position += new Vector3(-1, 0, 0);
-
-            if (CheckIsValidPosition())  // 존재하고 있는지 확인 
-                FindObjectOfType<Game>().UpdateGrid(this); // 공간계산
-            else
-                transform.position += new Vector3(1, 0, 0);
+            moveLeft();
         }   // 왼쪽 끝
 
-        else if (Input.GetKeyDown(KeyCode.UpArrow)) // 위쪽 화살표 입력
+        if (Input.GetKeyDown(KeyCode.UpArrow)) // 위쪽 화살표 입력
         {
-            if (allowRoatation) // 회전 가능
+            moveRotate();
+        } // 위쪽 끝
+
+         if (Input.GetKey(KeyCode.DownArrow) || Time.time-fall>=fallSpeed) // 아래쪽 화살표 입력 or 자동으로 한칸씩 떨어짐
+        {
+            moveDown();
+        } // 아래쪽 끝
+    }   // 함수 끝
+
+    // 키를 계속 누르고 있을 때, 블록이 아래로 내려가지 않는 문제 수정
+    void moveLeft() // 왼쪽 움직임
+    {
+        if (moveImmediateHorizontal) // 계속 누르고 있을 때
+        {
+            if (buttonDownWaitTimerHorizontal < buttonDownWaitMax)    // 딜레이 시간
             {
-                if (limitRoatiotion)    // 제한된 회전 -> S블록 등은 회전 제한
+                buttonDownWaitTimerHorizontal += Time.deltaTime;
+                return;
+            }
+
+            if (horizontalTimer < continuousHorizontalSpeed) // 움직인 시간(누른 시간)에 따라서 속력이 결정됨 
+            {
+                horizontalTimer += Time.deltaTime;
+                return; // 끝남을 의미함
+            }
+        }
+        if (!moveImmediateHorizontal)   // 한번만 누를 경우
+            moveImmediateHorizontal = true;
+
+        horizontalTimer = 0;    // 초기화
+
+        transform.position += new Vector3(-1, 0, 0);
+
+        if (CheckIsValidPosition())  // 존재하고 있는지 확인 
+            FindObjectOfType<Game>().UpdateGrid(this); // 공간계산
+        else
+            transform.position += new Vector3(1, 0, 0);
+    }
+
+    void moveRight()    // 오른쪽 움직임
+    {
+        if (moveImmediateHorizontal)    // 계속 누르고 있을 때
+        {
+            if (buttonDownWaitTimerHorizontal < buttonDownWaitMax)   // 딜레이 시간
+            {
+                buttonDownWaitTimerHorizontal += Time.deltaTime;
+                return;
+            }
+
+            if (horizontalTimer < continuousHorizontalSpeed) // 움직인 시간(누른 시간)에 따라서 속력이 결정됨 
+            {
+                horizontalTimer += Time.deltaTime;
+                return; // 끝남을 의미함
+            }
+        }
+        if (!moveImmediateHorizontal)   // 한번만 누를 경우
+            moveImmediateHorizontal = true;
+
+        horizontalTimer = 0;    // 초기화
+
+        transform.position += new Vector3(1, 0, 0);
+
+        if (CheckIsValidPosition()) // 존재하고 있는지 확인
+            FindObjectOfType<Game>().UpdateGrid(this); // 공간 계산
+        else
+            transform.position += new Vector3(-1, 0, 0);
+    }
+
+    void moveDown() // 아래로 움직임
+    {
+        if (moveImmediateVertical)   // 계속 누르고 있을 경우
+        {
+            if (buttonDownWaitTimerVertical < buttonDownWaitMax)    // 딜레이 시간
+            {
+                buttonDownWaitTimerVertical += Time.deltaTime;
+                return;
+            }
+
+            if (verticalTimer < continuousVerticalSpeed)   // 움직인 시간(누른 시간)에 따라서 속력이 결정됨
+            {
+                verticalTimer += Time.deltaTime;
+                return; // 끝남을 의미
+            }
+        }
+        if (!moveImmediateVertical) // 한번만 누를 경우
+            moveImmediateVertical = true;
+
+        verticalTimer = 0;  // 초기화
+
+        transform.position += new Vector3(0, -1, 0);    // 아래로 한 칸 이동
+
+        if (CheckIsValidPosition()) // 존재하고 있는지 확인 
+            FindObjectOfType<Game>().UpdateGrid(this);  // 공간계산
+        else
+        {
+            transform.position += new Vector3(0, 1, 0);
+
+            FindObjectOfType<Game>().DeleteRow();   // 행이 다 차있을 경우 행 삭제 실행
+
+            if (FindObjectOfType<Game>().CheckIsAboveGrid(this)) // 블록이 마지막에 도달했는지 검사
+            {
+                FindObjectOfType<Game>().GameOver();
+            }
+
+            // 나중에 음악 추가시 getkey로 변경!
+            // Game.current_score += individualScore;  // 놓는 속도에 따라 점수 계산 실행
+
+            enabled = false;    // 움직일 수 없게 하는 것!(바닥에 착지)
+            FindObjectOfType<Game>().SpawnNextTetromino();  // 다음 블록 자동 생성
+        }
+        fall = Time.time;   // 떨어지는 속도 변경
+                            // 시간 - 지난 시간 >= 떨어지는 속도
+    }
+
+    void moveRotate()   // 회전(위방향키)
+    {
+        if (allowRoatation) // 회전 가능
+        {
+            if (limitRoatiotion)    // 제한된 회전 -> S블록 등은 회전 제한
+            {
+                if (transform.rotation.eulerAngles.z >= 90)    // z축 회전 각이 90이상
+                    transform.Rotate(0, 0, -90);
+                else
+                    transform.Rotate(0, 0, 90);
+            }
+            else
+                transform.Rotate(0, 0, 90); // 90도 회전
+
+            if (CheckIsValidPosition()) // 존재하고 있는지 확인 
+                FindObjectOfType<Game>().UpdateGrid(this); // 공간계산
+            else
+            {
+                if (limitRoatiotion)
                 {
-                    if(transform.rotation.eulerAngles.z>=90)    // z축 회전 각이 90이상
+                    if (transform.rotation.eulerAngles.z >= 90)
                         transform.Rotate(0, 0, -90);
                     else
                         transform.Rotate(0, 0, 90);
                 }
                 else
-                    transform.Rotate(0, 0, 90); // 90도 회전
-
-               if (CheckIsValidPosition()) // 존재하고 있는지 확인 
-                    FindObjectOfType<Game>().UpdateGrid(this); // 공간계산
-                else
-                {
-                    if (limitRoatiotion)
-                    {
-                        if (transform.rotation.eulerAngles.z >= 90)
-                            transform.Rotate(0, 0, -90);
-                        else
-                            transform.Rotate(0, 0, 90);
-                    }
-                    else
-                        transform.Rotate(0, 0, -90);
-                }  
-             }
-        } // 위쪽 끝
-
-        else if (Input.GetKey(KeyCode.DownArrow) || Time.time-fall>=fallSpeed) // 아래쪽 화살표 입력 or 자동으로 한칸씩 떨어짐
-        {
-            if(moveImmediateVertical)   // 계속 누르고 있을 경우
-            {
-                if (buttonDownWaitTimer < buttonDownWaitMax)    // 딜레이 시간
-                {
-                    buttonDownWaitTimer += Time.deltaTime;
-                    return;
-                }
-
-                if (verticalTimer < continuousVerticalSpeed)   // 움직인 시간(누른 시간)에 따라서 속력이 결정됨
-                {
-                    verticalTimer += Time.deltaTime;
-                    return; // 끝남을 의미
-                }
+                    transform.Rotate(0, 0, -90);
             }
-            if (!moveImmediateVertical) // 한번만 누를 경우
-                moveImmediateVertical = true;
-
-            verticalTimer = 0;  // 초기화
-
-            transform.position += new Vector3(0, -1, 0);    // 아래로 한 칸 이동
-
-            if (CheckIsValidPosition()) // 존재하고 있는지 확인 
-                FindObjectOfType<Game>().UpdateGrid(this);  // 공간계산
-            else
-            {
-                transform.position += new Vector3(0, 1, 0);
-
-                FindObjectOfType<Game>().DeleteRow();   // 행이 다 차있을 경우 행 삭제 실행
-
-                if(FindObjectOfType<Game>().CheckIsAboveGrid(this)) // 블록이 마지막에 도달했는지 검사
-                {
-                    FindObjectOfType<Game>().GameOver();
-                }
-
-                // 나중에 음악 추가시 getkey로 변경!
-                // Game.current_score += individualScore;  // 놓는 속도에 따라 점수 계산 실행
-
-                enabled = false;    // 움직일 수 없게 하는 것!(바닥에 착지)
-                FindObjectOfType<Game>().SpawnNextTetromino();  // 다음 블록 자동 생성
-            }
-            fall = Time.time;   // 떨어지는 속도 변경
-            // 시간 - 지난 시간 >= 떨어지는 속도
-        } // 아래쪽 끝
-        // 스페이스바 -> 한번에 떨어지는 것 추가하기
-    }   // 함수 끝
+        }
+    }
 
     bool CheckIsValidPosition() // 존재 유무, 올바른 위치인지 판별
     {
